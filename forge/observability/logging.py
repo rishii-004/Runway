f05989dcb5e65a79e992d18138078fb79b985757
rishii-004 <rishikesh.inno@gmail.com>
2026-08-18 01:01:42 +0530

    chore: add structlog JSON logging setup

---
+++ forge/observability/logging.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+import structlog
+
+
+def setup_logging(level: int = logging.INFO) -> None:
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer(),
+        ],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
+
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=level,
+    )
